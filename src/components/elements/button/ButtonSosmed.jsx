0e7f4851a31431fc00e1f8a52be00d352289614a
dian ardiansyah <you@example.com>
2024-03-14 07:52:27 +0700

    transisi btn sosmed

--- src/components/elements/button/ButtonSosmed.jsx
+++ src/components/elements/button/ButtonSosmed.jsx
@@ -3,7 +3,7 @@ const ButtonSosmed = (props) => {
     const {href, icon} = props
     return(
         <a href={href}>
-          <FontAwesomeIcon icon={icon} className="scale-125" />
+          <FontAwesomeIcon icon={icon} className="scale-125 hover:scale-150 transition-all duration-600 ease-in-out" />
         </a>
     )
 }
